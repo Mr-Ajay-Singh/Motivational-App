f86b5aaafeb39a2ddf0aed51f247cfffaf6ce6d3
ajay <1rn18cs009.ajay@gmail.com>
2023-03-07 11:17:21 +0530

    check

--- app/src/main/java/com/invictus/motivationalquotes/data/quotes/BreakupQuotes.kt
+++ app/src/main/java/com/invictus/motivationalquotes/data/quotes/BreakupQuotes.kt
@@ -5,7 +5,8 @@ object BreakupQuotes {
     fun getQuotes():List<String>{
         return arrayListOf(
             "Keep your face to the sunshine and you cannot see a shadow.",
-            "In every day, there are 1,440 minutes. That means we have 1,440 daily opportunities to make a positive impact."
+            "In every day, there are 1,440 minutes. That means we have 1,440 daily opportunities to make a positive impact.",
+            ""
         )
     }
 
