0bb87cab6d8a6c076a58c6ef5881577363f33715
Mr-Ajay-Singh <1rn18cs009.ajay@gmail.com>
2023-12-07 10:22:36 +0530

    privacy policy link change

--- app/src/main/java/com/invictus/motivationalquotes/ui/settingScreens/SettingScreen.kt
+++ app/src/main/java/com/invictus/motivationalquotes/ui/settingScreens/SettingScreen.kt
@@ -8,14 +8,30 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Slider
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -49,14 +65,14 @@ fun SettingScreen(selectedPage: MutableState<MainScreenIdentifier>) {
                 R.drawable.privacy_policy,
                 Pair(
                     context.getString(R.string.privacy_policy),
-                    "https://doc-hosting.flycricket.io/motivational-quotes-privacy-policy/6a7b3356-05cf-4512-91c8-ad1ea31c4da3/privacy"
+                    "https://motivation-gratitude-achieve.blogspot.com/p/privacy-policy.html"
                 )
             ),
             Pair(
                 R.drawable.terms_conditions,
                 Pair(
                     context.getString(R.string.terms_and_conditions),
-                    "https://doc-hosting.flycricket.io/motivational-quotes-terms-of-use/34993005-e136-4af4-a062-2f6132dcbd27/terms"
+                    "https://motivation-gratitude-achieve.blogspot.com/p/terms-and-conditions.html"
                 )
             ),
             Pair(
